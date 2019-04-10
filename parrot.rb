32e8f57adfee1826912f3e00195f4aef8d916f8f
Robert <r_lee94@yahoo.com>
2019-04-10 05:24:34 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,13 +1,9 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
-# def parrot(phrase = "Squawk!")
-# puts "Squawk!"
-#   return phrase
-#   puts phrase(phrase)
+Create method `parrot` that outputs a given phrase and
+returns the phrase
+def parrot(phrase = "Squawk!")
+puts "Squawk!"
+  return phrase
+  puts phrase(phrase)
   
 end
 
-def parrot(phrase = "Squawk!")
-  puts phrase
-  return phrase
-end
